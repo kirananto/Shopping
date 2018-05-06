73dea0f4510caaf2da79a074111b303da971eda1
Kiran Anto <kirananto@gmail.com>
2018-05-06 23:04:04 +0530

    some small changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import OAuthCallback from '@/components/OAuthCallback'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/OAuthCallback',
+      name: 'OAuthCallback',
+      component: OAuthCallback
     }
-  ]
+  ],
+  mode: 'history'
 })
